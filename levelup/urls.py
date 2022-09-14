ec48bfb65ed02f3b386f64506386e94ceab922aa
Rose Cota <rosejcota@gmail.com>
2022-09-14 15:19:52 -0500

    Created GameTypeView and added list and retrieve methods

--- levelup/urls.py
+++ levelup/urls.py
@@ -1,10 +1,15 @@
 from django.contrib import admin
 from django.conf.urls import include
 from django.urls import path
-from levelupapi.views import register_user, login_user
+from rest_framework import routers
+from levelupapi.views import register_user, login_user, GameTypeView
+
+router = routers.DefaultRouter(trailing_slash=False)
+router.register('gametypes', GameTypeView, 'gametype')
 
 urlpatterns = [
     path('register', register_user),
     path('login', login_user),
     path('admin/', admin.site.urls),
+    path('', include(router.urls)),
 ]
