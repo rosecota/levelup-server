6dc069c8c97534ff19193910961cdea2c80b1128
Rose Cota <rosejcota@gmail.com>
2022-09-21 18:45:24 -0500

    remove old create game method comments

--- levelupapi/views/game.py
+++ levelupapi/views/game.py
@@ -40,26 +40,6 @@ class GameView(ViewSet):
         serializer = GameSerializer(games, many=True)
         return Response(serializer.data)
 
-    # def create(self, request):
-    #     """Handle POST operations
-
-    #     Returns
-    #         Response -- JSON serialized game instance
-    #     """
-    #     gamer = Gamer.objects.get(user=request.auth.user)
-    #     game_type = GameType.objects.get(pk=request.data["game_type"])
-
-    #     game = Game.objects.create(
-    #         title=request.data["title"],
-    #         maker=request.data["maker"],
-    #         number_of_players=request.data["number_of_players"],
-    #         skill_level=request.data["skill_level"],
-    #         gamer=gamer,
-    #         game_type=game_type
-    #     )
-    #     serializer = GameSerializer(game)
-    #     return Response(serializer.data)
-
     def create(self, request):
         """Handle POST operations
 
